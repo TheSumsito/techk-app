8c1269e68c20da5a34815022ebe86c91f4fa4b4e
TheSumsito <mjaral@outlook.com>
2021-02-11 19:51:09 -0300

    Create and instance dependence Paginate and include in view Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from "axios";
+import Pagination from 'react-js-pagination';
 
 //styles
 import "../assets/css/home.css"
@@ -14,14 +15,22 @@ export default class Home extends Component {
         data: [],
         count: 0,
         status: false,
-        err: ''
+        err: '',
+        activePage: 0
+    }
+
+    constructor(props){
+        super(props)
+        this.state = {
+            activePage: 1
+        }
     }
 
     txtSearch = React.createRef()
     getSearchFilms = (e) => {
         e.preventDefault()
         const value = this.txtSearch.current.value
-
+        
         axios({
             method: 'POST',
             url: 'http://www.omdbapi.com/?s='+value+'&apikey=f9a88492'
@@ -32,6 +41,11 @@ export default class Home extends Component {
                     data: x.data.Search,
                     count: x.data.totalResults
                 })
+            } else {
+                this.setState({
+                    data: [],
+                    status: false
+                })
             }
         }).catch((err)=>{
             this.setState({
@@ -39,8 +53,37 @@ export default class Home extends Component {
                 err: err
             })
         })
-
     }
+
+    changePage(pageNumber){
+        const value = this.txtSearch.current.value
+
+        axios({
+            method: 'POST',
+            url: 'http://www.omdbapi.com/?s='+value+'&page='+pageNumber+'&apikey=f9a88492'
+        }).then((x) => {
+            if(x.data.Response !== 'False'){
+                this.setState({
+                    status: true,
+                    data: x.data.Search,
+                    activePage: pageNumber
+                })
+            } else {
+                this.setState({
+                    data: [],
+                    status: false,
+                    count: 0
+                })
+            }
+        }).catch((err)=>{
+            this.setState({
+                status: false,
+                err: err
+            })
+        })
+    }
+
+
     render() {
         return (
             <div className="container">
@@ -63,6 +106,20 @@ export default class Home extends Component {
                             data={this.state.data}
                             status={this.state.status}
                         />
+                    </div>
+                    <div className="paginate">
+                    {
+                        this.state.status &&
+                            <Pagination 
+                                activePage={this.state.activePage}
+                                itemsCountPerPage={10}
+                                totalItemsCount={parseInt(this.state.count)}
+                                pageRangeDisplayed={5}
+                                itemClass="page-item"
+                                linkClass="page-link"   
+                                onChange={this.changePage.bind(this)}
+                            />
+                    }
                     </div>
                 </form>
             </div>
